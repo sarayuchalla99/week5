3c6a952bfb50723790f345cb40f0c21287560966
sarayuchalla99 <sarayuchalla86@gmail.com>
2026-08-14 14:51:09 +0530

    Update and rename factorial.java to Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
